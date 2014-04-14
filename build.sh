5388cac1f4c432fecb267f6024204ceb70424243
Ernesto Celis <developer@celisdelafuente.net>
2014-04-14 17:51:30 -0500

    * FIX deleted files from pjsip library

--- build.sh
+++ build.sh
@@ -24,17 +24,17 @@ fi
 
 cd $CWD/$PJPDIR
 make distclean
-rm -f pjmedia/include/pjmedia/config_auto.h
-rm -f pjmedia/include/pjmedia-codec/config_auto.h
-rm -f pjmedia/build/os-auto.mak
-rm -f pjlib/include/pj/config_site.h 
-rm -f pjlib/include/pj/compat/os_auto.h
-rm -f pjlib/include/pj/compat/m_auto.h
-rm -f pjlib/build/os-auto.mak
-rm -f pjlib-util/build/os-auto.mak
-rm -f build/os-auto.mak
-rm -f build/cc-auto.mak
-rm -f build.mak
+#rm -f pjmedia/include/pjmedia/config_auto.h
+#rm -f pjmedia/include/pjmedia-codec/config_auto.h
+#rm -f pjmedia/build/os-auto.mak
+#rm -f pjlib/include/pj/config_site.h 
+#rm -f pjlib/include/pj/compat/os_auto.h
+#rm -f pjlib/include/pj/compat/m_auto.h
+#rm -f pjlib/build/os-auto.mak
+#rm -f pjlib-util/build/os-auto.mak
+#rm -f build/os-auto.mak
+#rm -f build/cc-auto.mak
+#rm -f build.mak
 if [[ $ENABLE_VIDEO == 'y' ]] ; then
   ./configure
 else
