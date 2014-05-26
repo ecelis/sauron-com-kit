ad9a1ea79b5f7d085e242bc8b2df8de9cf65c0e5
Ernesto Celis <developer@celisdelafuente.net>
2014-05-26 17:36:21 -0500

    * FIX do not set sound dev unless strictly neccesary.

--- ve-phone/vephone.py
+++ ve-phone/vephone.py
@@ -247,7 +247,7 @@ try:
     # Init pjsua with default config
     lib.init(log_cfg = pj.LogConfig(level=LOG_LEVEL, callback=log_cb))
     # Set sound device TODO in vc.py
-    lib.set_snd_dev(0,0)
+    #lib.set_snd_dev(0,0)
     # Create UDP transport which listens to any available port
     transport = lib.create_transport(pj.TransportType.UDP)
     # Start the library
